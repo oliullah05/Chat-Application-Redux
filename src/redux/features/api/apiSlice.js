929245fabd931aec48585b637b84b0c4f9fd5b10
oliullah05 <mdolihasan@gmail.com>
2024-01-18 16:38:04 +0600

    in every api request , headers set done in baseapi using prepareHeaders property

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,11 +1,22 @@
-import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-console.log(process.env.REACT_APP_API_URL,44)
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+console.log(process.env.REACT_APP_API_URL, 44)
 export const apiSlice = createApi({
-    
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({baseUrl:"http://localhost:9000/"}),
-    tagTypes:[],
-    endpoints:(builder)=>({
+
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:9000/",
+        prepareHeaders: async (headers, { getState, endpoint }) => {
+            const token = getState()?.auth?.accessToken
+            console.log(token);
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`)
+            }
+            return headers
+        }
+
+    }),
+    tagTypes: [],
+    endpoints: (builder) => ({
 
     })
 })
