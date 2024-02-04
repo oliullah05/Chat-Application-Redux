e9b64c5606967256a66a29ea62f40a8adba3caa2
oliullah05 <mdolihasan@gmail.com>
2024-02-04 18:53:58 +0600

    Emitting Events from server

--- server/server.js
+++ server/server.js
@@ -5,8 +5,25 @@ const http = require("http")
 const app = express()
 const server = http.createServer(app);
 const io = require('socket.io')(server)
-global.io=io;
+global.io = io;
 const router = jsonServer.router("db.json");
+
+
+
+//response middlewar
+router.render = (req, res) => {
+    const path = req.path;
+    const method = req.method;
+    if (path.includes("/conversations") && (method === "POST" || method === "PATCH")) {
+        //emmit socket event
+        io.emmit("conversation",{
+        data:res.locals.data
+        })
+
+    }
+    res.json(res.locals.data)
+}
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
