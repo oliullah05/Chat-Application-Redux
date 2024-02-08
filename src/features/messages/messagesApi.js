7e7d3f58cd79679d2c1deb3d45a727dc10162a8c
oliullah05 <mdolihasan@gmail.com>
2024-02-08 21:53:28 +0600

    initial chat application done

--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -7,35 +7,35 @@ export const messagesApi = apiSlice.injectEndpoints({
                 `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
         }),
         getMoreMessages: builder.query({
-            query: ({id,page}) =>
+            query: ({ id, page }) =>
                 `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=${page}&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
 
-                async onQueryStarted({ id }, { queryFulfilled, dispatch }) {
-                    try {
-                        const conversations = await queryFulfilled;
-                      
-                        if (conversations?.data?.length > 0) {
-                            // update conversation cache pessimistically start
-                            dispatch(
-                                apiSlice.util.updateQueryData(
-                                    "getMessages",
-                                    id.toString(),
-                                    (draft) => {
-                                        // console.log("under tnhe draft");
-                                        // console.log(JSON.stringify(conversations,8888));
-                                        return [
-                                                ...draft,
-                                                ...conversations.data,
-                                            ]
-                                           
-                                        
-                                    }
-                                )
-                            );
-                            // update messages cache pessimistically end
-                        }
-                    } catch (err) {}
-                },
+            async onQueryStarted({ id }, { queryFulfilled, dispatch }) {
+                try {
+                    const conversations = await queryFulfilled;
+
+                    if (conversations?.data?.length > 0) {
+                        // update conversation cache pessimistically start
+                        dispatch(
+                            apiSlice.util.updateQueryData(
+                                "getMessages",
+                                id.toString(),
+                                (draft) => {
+                                    // console.log("under tnhe draft");
+                                    // console.log(JSON.stringify(conversations,8888));
+                                    return [
+                                        ...draft,
+                                        ...conversations.data,
+                                    ]
+
+
+                                }
+                            )
+                        );
+                        // update messages cache pessimistically end
+                    }
+                } catch (err) { }
+            },
         }),
         addMessage: builder.mutation({
             query: (data) => ({
@@ -47,4 +47,4 @@ export const messagesApi = apiSlice.injectEndpoints({
     }),
 });
 
-export const { useGetMessagesQuery, useAddMessageMutation,useGetMoreMessagesQuery } = messagesApi;
+export const { useGetMessagesQuery, useAddMessageMutation, useGetMoreMessagesQuery } = messagesApi;
